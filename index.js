b9c67ba8946eda66bd0419f8fa613a7818a484b0
Chintan Radia <radiachints1512@gmail.com>
2015-08-27 16:36:58 +0530

    xo error resolved

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 var got = require('got');
 
 module.exports = function (userName, callback) {
-	if(!(typeof userName === 'string' && userName.length !== 0)) {
+	if (!(typeof userName === 'string' && userName.length !== 0)) {
 		throw new Error('User Name required');
 	}
-	got('https://typeahead-js-twitter-api-proxy.herokuapp.com/demo/search?q=' + userName.toLowerCase(), function (err, data, res) {
+	got('https://typeahead-js-twitter-api-proxy.herokuapp.com/demo/search?q=' + userName.toLowerCase(), function (err, data) {
 		if (err) {
 			callback(err);
 			return;
 		}
 		var jsonArray = JSON.parse(data);
-		(jsonArray.length === 0) ? callback(null, true) : callback(null, false);
+		jsonArray.length ? callback(null, true) : callback(null, false);
 	});
 };
