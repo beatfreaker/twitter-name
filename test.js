a56693f0819a86d87c20ca32008a944ecec29337
Chintan Radia <radiachints1512@gmail.com>
2015-08-27 16:45:54 +0530

    xo error solved

--- test.js
+++ test.js
@@ -3,14 +3,14 @@ var test = require('tape');
 var twitterName = require('./');
 test('Testing Twitter name availability', function (t) {
 	t.plan(1);
-	twitterName ('dserijdk', function(err, isAvailable) {
-        t.equal(true, isAvailable, 'Both value matched');
+	twitterName('dserijdk', function (err, isAvailable) {
+		t.equal(true, isAvailable, 'Both value matched');
 	});
 });
 
 test('Testing Twitter name unavailability', function (t) {
 	t.plan(1);
-	twitterName ('chintsradia', function(err, isAvailable) {
-	    t.equal(false, isAvailable, 'Both value matched');
+	twitterName('chintsradia', function (err, isAvailable) {
+		t.equal(false, isAvailable, 'Both value matched');
 	});
 });
